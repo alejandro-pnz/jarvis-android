6cd357bc900507f0aed74448df7d9b89bd9fd333
Александр Кожевников <pnz.alexander.kozhevnikov@gmail.com>
2015-10-13 13:15:53 +0300

    добавлено подключение к серверу

--- app/src/main/java/com/coffeinum/jarvis/MainActivity.java
+++ app/src/main/java/com/coffeinum/jarvis/MainActivity.java
@@ -1,17 +1,45 @@
 package com.coffeinum.jarvis;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import ai.api.AIConfiguration;
+import ai.api.AIListener;
+import ai.api.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity implements AIListener{
+
+    Button voiceControlButton;
+    AIService aiService;
+    final AIConfiguration config = new AIConfiguration( "9ad76731-7552-47f8-a3ef-7f906f5fbf00",
+            "4f24c29c52a64232bc5d2b6057cf93f3",
+            AIConfiguration.SupportedLanguages.English,
+            AIConfiguration.RecognitionEngine.System);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aiService = AIService.getService(this, config);
+
+        aiService.setListener(this);
+
+        voiceControlButton = (Button)findViewById(R.id.voice_control_btn);
+        voiceControlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aiService.startListening();
+            }
+        });
     }
 
     @Override
@@ -34,5 +62,35 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResult(AIResponse aiResponse) {
+        Toast.makeText(this, aiResponse.getResult().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(AIError aiError) {
+        Toast.makeText(this, R.string.voice_error, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAudioLevel(float v) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
